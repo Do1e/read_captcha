cdc7989210f9dad09950d3f6c6b66fd9b942ee25
do1e <dpj.email@qq.com>
2022-05-28 13:05:51 +0800

    add annotation

--- readcap.py
+++ readcap.py
@@ -5,8 +5,8 @@ import numpy as np
 import sys
 
 saveDir = "./pic/"
-
 capurl = "https://authserver.nju.edu.cn/authserver/captcha.html"
+
 session = requests.Session()
 
 while 1:
@@ -16,14 +16,14 @@ while 1:
 	res = ocr.classification(r.content)
 	cv2.imshow(res, img)
 	key = cv2.waitKey(0)
-	if key == 13:
+	if key == 13:		# Enter
 		with open(saveDir + res + ".jpg", "wb") as f:
 			f.write(r.content)
-	elif key == 27:
+	elif key == 27:		# Esc
 		exit(0)
 	else:
 		flag = False
-		if(key == 225 or key == 226):
+		if(key == 225 or key == 226):	# Shift
 			flag = True
 			res = ""
 		else:
@@ -33,19 +33,19 @@ while 1:
 		
 		while 1:
 			key = cv2.waitKey(0)
-			if key == 225 or key == 226:
+			if key == 225 or key == 226:	# Shift
 				flag = True
-			elif key >= 97 and key <= 122:
+			elif key >= 97 and key <= 122:	# a-z
 				if flag:
 					res += chr(key).upper()
 					flag = False
 				else:
 					res += chr(key)
-			elif key >= 49 and key <= 57:
+			elif key >= 49 and key <= 57:	# 0-9
 				res += chr(key)
-			elif key == 8:
+			elif key == 8:					# Backspace
 				res = res[:-1]
-			elif key == 13:
+			elif key == 13:					# Enter
 				break
 			print("\b\b\b\b\b\b\b" + res, end="")
 			sys.stdout.flush()
